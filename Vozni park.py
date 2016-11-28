3c7edb576c1918a6029ac94cc60ee7db451d5c6a
Jaka Marinšek <jaka.marinsek@gmail.com>
2016-11-28 21:27:25 +0100

    popravki

--- Vozni park.py
+++ Vozni park.py
@@ -8,14 +8,12 @@ class Cars:
     def get_full_list(self):
         return self.znamka + " " + self.model + " " + self.st_km + " " + self.leto_ser
 
-
 def list_all_cars(cars):
     for index, vehicle in enumerate(cars):
         print "ID: " + str(index)
         print vehicle.get_full_list()
 
         print ""
-
 
 def add_new_cars(cars):
     znamka = raw_input("Vnesite znamko avtomobila: ")
@@ -26,26 +24,24 @@ def add_new_cars(cars):
     new = Cars(znamka=znamka, model=model, st_km=st_km, leto_ser=leto_ser)
     cars.append(new)
 
-    print ""  # empty line
+    print ""
     print new.get_full_list() + " was successfully added to your car list."
 
 def edit_cars(cars):
-    print "Select the number of the car you'd like to edit: "
+    print "List of the cars available for edit: "
 
-    for index, p in enumerate(cars):
-        print str(index) + ") " + add_new_cars()
+    for index, vehicle in enumerate(cars):
+        print str(index) + ") " + vehicle.get_full_list()
 
-    print ""  # empty line
+    print ""
     selected_id = raw_input("What car would you like to edit? (enter ID number): ")
     selected_car = cars[int(selected_id)]
+    print""
 
     new_km_num = raw_input("Please enter new number of km for %s: " % selected_car.get_full_list())
     selected_car.st_km = new_km_num
-
-    print ""  # empty line
     print "Number of km updated."
-    # ... you can do the same for other fields.
-
+    print ""
 
 def delete_car(cars):
     print "Select the number of the car you'd like to delete:"
@@ -53,34 +49,33 @@ def delete_car(cars):
     for index, vehicle in enumerate(cars):
         print str(index) + ") " + vehicle.get_full_list()
 
-    print ""  # empty line
+    print ""
     selected_id = raw_input("What car would you like to delete? (enter ID number): ")
     selected_car = cars[int(selected_id)]
 
     cars.remove(selected_car)
-    print ""  # empty line
+    print ""
     print "Car was successfully removed from your contact list."
 
 
 def main():
     print "Welcome to your Car List"
 
-    # let's add some contacts in our contact list so it's not empty
-    vozilo_1 = Cars(znamka="bmw", model="320i", st_km="14000", leto_ser="1979")
-    cars = [vozilo_1]
+    vozilo = Cars(znamka="bmw", model="320i", st_km="14000", leto_ser="1979")
+    cars = [vozilo]
 
     while True:
-        print ""  # empty line
+        print ""
         print "Please choose one of these options:"
         print "a) See all your cars"
         print "b) Add a new car"
         print "c) Edit a car"
         print "d) Delete a car"
         print "e) Quit the program."
-        print ""  # empty line
+        print ""
 
         selection = raw_input("Enter your selection (a, b, c, d or e): ")
-        print ""  # empty line
+        print ""
 
         if selection.lower() == "a":
             list_all_cars(cars)
@@ -93,9 +88,14 @@ def main():
         elif selection.lower() == "e":
             print "Thank you for using Car List. Goodbye!"
             break
-        else:
-            print "Sorry, I didn't understand your selection. Please try again."
-            continue
 
 if __name__ == "__main__":
     main()
+
+with open("Seznam.txt", "w+") as vozilo:
+    print "Seznam vozil --> prenos v zunanjo dadoteko:"
+    vozilo.write("Seznam vozil:\n")
+    for item in vozilo:
+            print "- " + item
+            vozilo.write("- " + item + "\n")
+vozilo.close()
